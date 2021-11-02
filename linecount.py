726e614a6b3bf37c5a1fb883a4668a135a3a6103
bmclay <93384439+bmclay@users.noreply.github.com>
2021-11-02 08:44:18 -0400

    Update linecount.py

--- linecount.py
+++ linecount.py
@@ -1,7 +1,11 @@
 # Open a file to work with
 filename = input('Specify the file path for the file to open: ')
+try:
+    fhand = open(filename)
+except:
+    print('File cannot be opened:', filename)
+    quit()
 
-fhand = open(filename)
 # Prime the loop to start at 0
 count = 0
 for line in fhand:
