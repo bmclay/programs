6c8765189a4deb6f6f4c339bad7dc29d12033564
bmclay <93384439+bmclay@users.noreply.github.com>
2021-11-10 15:02:36 -0500

    Add files via upload

---
+++ programs-main/JAVASCRIPT/script.js
@@ -0,0 +1,22 @@
+//  The program that picks where we are going to eat lunch!
+
+//  Make the lists
+const styles = ['Pizza', 'Chinese', 'American', 'Italian', 'Mexican', 'Fast Food'];
+
+// Lists of each resteraunt for each cuisine style
+const pizza = ['Pizza Joes', 'Foxs', 'Pizza Pie', 'Pizza Plus', 'Vocellis', 'Dominoes', 'Tanis', 'Anna Maries'];
+const chinese = ['China Gourmet', 'China One', 'China Palace'];
+const american = ['Monroe Hotel', 'Rachels Roadhouse', 'Chop Shop', 'Burger Hut', 'Texas Roadhouse', 'Upper Crust', 'Cannella Cafe', 'Hoagie Shop', 'Chilis', 'Applebees'];
+const italian = ['Villa Grande', 'Mama Rosas'];
+const mexican = ['Rey Azteca', 'Compadres'];
+const fastfood = ['Mcdonalds', 'Arbys', 'Wendys', 'Burger King', 'Subway', 'KFC', 'Dairy Queen', 'Taco Bell'];
+
+let choice ='';
+
+if (choice === 'q') {
+    console.log('Goodbye');
+}
+
+if (choice === '1') {
+    console.log()
+}
