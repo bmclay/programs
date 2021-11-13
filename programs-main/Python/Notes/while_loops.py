343372996dcb8174fd6af967f0b983cbe12fb1e9
bmclay <93384439+bmclay@users.noreply.github.com>
2021-11-13 14:36:54 -0500

    Delete programs-main/Python/Notes directory

--- programs-main/Python/Notes/while_loops.py
+++
@@ -1,8 +0,0 @@
-# While something is true do something
-i = 0
-while i < 10:
-    print('run')
-# add one to variable i so it increments by one each time it runs. Program stops after 10 loops.    
-    i += 1
-    if i == 10:
-        break
